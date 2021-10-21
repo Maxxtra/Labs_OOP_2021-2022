3c64fbd6e669f070766011ab06f496bafbe29cb0
Maxxtra <83414814+Maxxtra@users.noreply.github.com>
2021-10-21 10:02:27 +0300

    Update Problema2.java

--- Problema2.java
+++ Problema2.java
@@ -5,8 +5,8 @@ public class Problema2 {
             System.out.println(sir);
         }
         public static void main(String args[]) {
-            Problema2 obiect = new Problema2(); // creare obiect de tip Problema2
-            obiect.print("Test"); //apelare metoda print
+            Problema2 obiect = new Problema2();
+            obiect.print("Test");
         }
 
 }
