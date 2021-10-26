6f032b3929f7ba0f59fd308a3e0415527e29a431
Maxxtra <83414814+Maxxtra@users.noreply.github.com>
2021-10-26 14:42:37 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,45 @@
-public class main {
-    Problema2 obiect = new Problema2(); // creare obiect de tip Problema2
-    obiect.print("Test"); //apelare metoda print
+public class Main {
+
+    public static void main(String[] args) {
+        //task 1
+
+        System.out.println("TASK 1 ==================================================");
+        double pi = 3.14;
+        Paralelogram paralelogram = new Paralelogram(2, 3, pi / 6, 5 * pi / 6);
+        Patrat patrat = new Patrat(20, pi / 2);
+        Romb romb = new Romb(1, pi / 6, 5 * pi / 6);
+        Dreptunghi dreptunghi = new Dreptunghi(20, 30, pi / 2);
+
+        System.out.println(dreptunghi.arie());
+        System.out.println(patrat.arie());
+        System.out.println(paralelogram.arie());
+        System.out.println(romb.arie());
+
+        //task 2
+
+        System.out.println("TASK 2 ==================================================");
+        SortedArray sortedArray = new SortedArray();
+
+        sortedArray.addElement(3);
+        sortedArray.addElement(1);
+        sortedArray.addElement(6);
+        sortedArray.addElement(2);
+
+        System.out.println(sortedArray.toString());
+
+        MyStack myStack = new MyStack();
+
+        myStack.push(3);
+        myStack.push(1);
+        myStack.push(5);
+        myStack.push(7);
+
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+
+        //task 3
+
+    }
 }
